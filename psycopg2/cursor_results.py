7ec8d4fa402eafb84c54ac444c33f739a2bb683f
Ivan Novick <inovick@pivotal.io>
2019-11-28 17:18:03 -0800

    update cursor queries

--- psycopg2/cursor_results.py
+++ psycopg2/cursor_results.py
@@ -9,12 +9,16 @@ try:
     cursor.execute("DROP TABLE if exists food")
     cursor.execute("CREATE TABLE food(name text, about text, price float)")
     cursor.execute("INSERT into food values('banana', 'long yellow sweet fruit', '.50')")
-    cursor.execute("INSERT into food values('lemon', 'sour citrus fruit', '1.05')")
+    cursor.execute("INSERT into food values('lemon',  'sour citrus fruit', '1.05')")
+    cursor.execute("INSERT into food values('hotdog', 'long thing meat sandwich', '2.50')")
+    cursor.execute("INSERT into food values('corndog','carnival food', '3.00')")
+    cursor.execute("INSERT into food values('coffee', 'hot beverage', '2.75')")
     cursor.execute("INSERT into food values('hotdog', 'long thing meat sandwich', '2.50')")
     cursor.execute("SELECT * FROM food")
 
     print(cursor.description)
     print("Row Count: ", cursor.rowcount)
+
     for record in cursor:
         print (record)
 
---
+++ psycopg2/execute_many.py
@@ -0,0 +1,35 @@
+import psycopg2
+
+food = (
+    ('banana', 'long yellow sweet fruit', '.50'),
+    ('lemon',  'sour citrus fruit', '1.05'),
+    ('hotdog', 'long thing meat sandwich', '2.50'),
+    ('corndog','carnival food', '3.00'),
+    ('coffee', 'hot beverage', '2.75'),
+    ('hotdog', 'long thing meat sandwich', '2.50')
+)
+ 
+try:
+    connection = psycopg2.connect(database = "postgres")
+
+    cursor = connection.cursor()
+    cursor.execute("DROP TABLE if exists food")
+    cursor.execute("CREATE TABLE food(name text, about text, price float)")
+
+    query = "INSERT INTO food (name, about, price) VALUES (%s, %s, %s)"
+    cursor.executemany(query, food)
+    connection.commit()
+
+    cursor.execute("SELECT * FROM food")
+    print("SELECT * FROM food;")
+    for record in cursor:
+        print (record)
+
+
+except (Exception, psycopg2.Error) as error :
+    print ("Error while connecting to PostgreSQL", error)
+finally:
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
